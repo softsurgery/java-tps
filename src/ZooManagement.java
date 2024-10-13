5adfd18a6bea55d66c7ac41581c24f7af5823059
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2024-10-13 14:16:19 +0100

    Instruction 2

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,7 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class ZooManagement {
-    public static void main(String[] args) {
-        int nbCages = 20;
-        String zooName = "my_zoo";
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Entrez le nombre des cage : ");
+        int nbCages = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.println("Entrez le nom de zoo : ");
+        String zooName = scanner.nextLine();
+
         System.out.println(zooName + " comporte " + nbCages + " cages");
     }
 }

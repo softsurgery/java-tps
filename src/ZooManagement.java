946219d2af26646a5331db3e2615ef08b9fd689c
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2024-10-13 13:49:46 +0100

    Instruction 1

---
+++ src/ZooManagement.java
@@ -0,0 +1,7 @@
+public class ZooManagement {
+    public static void main(String[] args) {
+        int nbCages = 20;
+        String zooName = "my_zoo";
+        System.out.println(zooName + " comporte " + nbCages + " cages");
+    }
+}

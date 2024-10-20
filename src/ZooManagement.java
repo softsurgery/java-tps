954191a61ebf63be5d9ccc88f36672ebf05e778d
Houssem Eddine Chibouni <109615722+softsurgery@users.noreply.github.com>
2024-10-20 13:25:18 +0100

    Instruction 2 + Verifications

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,17 +1,44 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class ZooManagement {
+    static boolean isCharacterIncluded(char c, char b,char e){
+        return c >= b && c <= e;
+    }
+
+    static boolean isAlpha(String string){
+        int i = 0;
+        string = string.toUpperCase();
+        boolean test = true;
+        while(i < string.length() && test){
+            if (isCharacterIncluded(string.charAt(i),'A','Z')) i = i + 1;
+            else test = false;
+        }
+        return i == string.length();
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Entrez le nombre des cage : ");
-        int nbCages = scanner.nextInt();
+        int nbCages;
+        do{
+            System.out.println("Entrez le nombre des cage : ");
+            nbCages = scanner.nextInt();
+            if(nbCages < 0){
+                System.out.println("Veuillez saisir un nombre positif.");
+            }
+        }while(nbCages < 0);
 
         scanner.nextLine();
 
-        System.out.println("Entrez le nom de zoo : ");
-        String zooName = scanner.nextLine();
+        String zooName;
+        do{
+            System.out.println("Entrez le nom de zoo : ");
+            zooName = scanner.nextLine();
+            if (!isAlpha(zooName)){
+                System.out.println("Veuillez saisir un nom alphabetique.");
+            }
+        }while(!isAlpha(zooName));
+
 
         System.out.println(zooName + " comporte " + nbCages + " cages");
     }
